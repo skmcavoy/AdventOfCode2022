2e30f5a037e3555bdaa004d3691157ce985b3963
Steve McAvoy <steve@dynamixware.com>
2022-12-06 11:28:13 -0500

    Day 6 Part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filename = "input.txt";
+        int i=0;
+        bool found=false;
+        bool part2 = false;
+        string? textLine = string.Empty;
+        StreamReader streamReader = new StreamReader(filename);        
+        
+        if (!streamReader.EndOfStream)
+        {
+            textLine = streamReader.ReadToEnd();        
+        }
+        streamReader.Close();
+        for(i=0;(i<textLine.Length-4) && (!found);i++){
+             if (isUnique(textLine.Substring(i,4))){
+                found=true;
+                i+=3;
+             }
+        }
+        
+        System.Console.WriteLine("Found {0} at {1}",found,i);
+        
+                
+        
+    }
+
+    private static bool isUnique(string txt){
+        List<char> l = new List<char>(txt.ToCharArray());
+        return(l.Distinct().Count()==txt.Length);
+    }
+}
