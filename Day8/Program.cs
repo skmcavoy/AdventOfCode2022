9d3966d6585a2a9b525a0dc62aa45f9c91558d6f
Steve McAvoy <steve@dynamixware.com>
2022-12-13 11:43:47 -0500

    Day 8 Part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -17,6 +17,7 @@
         }
 
         Console.WriteLine($"Part 1: {findVisibleFromOutside(treeList)} Visible");
+        Console.WriteLine($"Part 2: {findBestScore(treeList)} Best Score");
     }
 
     private static int findVisibleFromOutside(List<List<int>> treeGrid)
@@ -47,4 +48,38 @@
         }
         return visCount;
     }
+    private static int findBestScore(List<List<int>> treeGrid){
+        int score =0;
+        for (int i = 1; i < treeGrid.Count()-1; i++)
+        {
+            if (!(i == 0 | i == treeGrid.Count() - 1))
+            {
+                for (int y = 0; y < treeGrid[i].Count(); y++)
+                {
+                    if (!(y == 0 | y == treeGrid[i].Count() - 1)){
+                        int left= treeGrid[i].FindLastIndex(y-1,y,x=>x>=treeGrid[i][y]);
+                        left= left == -1 ? y:y-left;
+
+                        int right = treeGrid[i].FindIndex(y+1,x=>x>=treeGrid[i][y]);
+                        right = right == -1? treeGrid[0].Count() - y -1 : right-y;
+
+                        int up = treeGrid.Select(x=>x[y]).ToList().FindLastIndex(i-1,i,x=>x>=treeGrid[i][y]);
+                        up= up == -1 ? i: i-up;
+
+                        int down =treeGrid.Select(x=>x[y]).ToList().FindIndex(i+1,x=>x>=treeGrid[i][y]);
+                        down = down ==-1 ? treeGrid.Count() -i -1 : down-i;
+
+
+                       // Console.WriteLine($"{i},{y}: {treeGrid[i][y]} | left={left}, right={right}, up={up}, down={down}");
+                        int tempScore = left*right*up*down;
+                        if (tempScore>score){
+                            score=tempScore;
+                        }
+                    }                                         
+                }                
+            }
+        }
+        return score;
+ 
+    }    
 }
